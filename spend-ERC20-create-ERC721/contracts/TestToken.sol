8b9fb665e00fc02eba23a7f75deaf5eb3e540e63
Faru Nuri Sönmez <info@farunurisonmez.com>
2021-11-09 15:18:58 +0300

    Solidity contract updated.
    ...
    
    Co-Authored-By: Faru Nuri Sönmez <info@farunurisonmez.com>

--- spend-ERC20-create-ERC721/contracts/TestToken.sol
+++ spend-ERC20-create-ERC721/contracts/TestToken.sol
@@ -10,25 +10,13 @@ contract SpenderRole is ERC20, AccessControl {
     
     event SpenderAdded(address indexed account);
     event SpenderRemoved(address indexed account);
-  
+    
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
-     
+    
     constructor() ERC20("TEST Token", "TEST") {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
         _addSpender(msg.sender);
-    }
-    
-    modifier onlySpender(){
-        require(isSpender(msg.sender));
-        _;
-    }
-    
-    function isSpender(address account) public view returns (bool){
-        return hasRole(BURNER_ROLE, msg.sender);
-    }
-    
-    function addSpender(address account) public onlySpender {
-        _addSpender(account);
     }
     
     function _addSpender(address account) internal {
@@ -36,14 +24,6 @@ contract SpenderRole is ERC20, AccessControl {
         emit SpenderAdded(account);
     }
     
-    function renounceSpender() public {
-        _removeSpender(msg.sender);
-    }
-    
-    function _removeSpender(address account) internal {
-        revokeRole(BURNER_ROLE,account);
-        emit SpenderRemoved(account);
-    }
 }
 
 /** 
@@ -51,18 +31,7 @@ contract SpenderRole is ERC20, AccessControl {
  **/
  
  contract ERC20Spendable is SpenderRole {
-    
-    /**
-    * @dev Function to mint tokens
-    * @param from The address that will spend the tokens
-    * @param value The amount of tokens to spend
-    * @return A boolean that indicates if the operation was successful
-    **/
-   
-    function spend(address from, uint256 value) public onlySpender returns(bool) {
-        _burn(from, value);
-        return true;
-    }
+
 }
 
 contract TestToken is SpenderRole {
