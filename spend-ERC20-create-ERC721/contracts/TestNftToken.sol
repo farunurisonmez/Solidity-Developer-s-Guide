a24a02b152d40a6b4e46dc9f6972b33dd54fecdc
Faru Nuri Sönmez <info@farunurisonmez.com>
2021-11-28 16:33:20 +0300

    Solidity contract updated.
    ...

--- spend-ERC20-create-ERC721/contracts/TestNftToken.sol
+++ spend-ERC20-create-ERC721/contracts/TestNftToken.sol
@@ -1,40 +1,75 @@
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/access/AccessControl.sol";
 
-import {ERC20Spendable} from "./TestToken.sol";
+/**
+ * This is an access control role for entities that may spend tokens
+ **/
+contract SpenderRole is ERC20, AccessControl {
+    
+    event SpenderAdded(address indexed account);
+    event SpenderRemoved(address indexed account);
+    
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+    
+    constructor() ERC20("Test Token", "TEST") {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _addSpender(msg.sender);
+    }
+    
+    modifier onlySpender(){
+        require(isSpender(msg.sender));
+        _;
+    }
+    
+    function isSpender(address account) public view returns (bool){
+        return hasRole(BURNER_ROLE, account);
+    }
+    
+    function addSpender(address account) public onlySpender {
+        _addSpender(account);
+    }
+    
+    function _addSpender(address account) internal {
+        grantRole(BURNER_ROLE,account);
+        emit SpenderAdded(account);
+    }
+    
+     function renounceSpender() public {
+        _removeSpender(msg.sender);
+    }
+    
+    function _removeSpender(address account) internal {
+        revokeRole(BURNER_ROLE,account);
+        emit SpenderRemoved(account);
+    }
+    
+    function decimals() public view virtual override returns (uint8) {
+        return 0;
+    }
 
-contract TestToken is Ownable {
-    uint256 _mintingPrice;
-    
-    ERC20Spendable _mintingCurrency;
-    
-    uint256 nextCertificateId = 1;
-    
-    mapping(uint256 => bytes32) certificateDataHashes;
-    
-    
-    function hashForToken(uint256 tokenId) external view returns (bytes32) {
-        return certificateDataHashes[tokenId];
+    function spend2(address from, uint256 value) public onlySpender returns(bool) {
+        _burn(from, value);
+        return true;
     }
-    
-    function mintingPrice() external view returns (uint256) {
-        return _mintingPrice;
+}
+
+/** 
+ * @dev ERC20 spender logic
+ **/
+ 
+ contract ERC20Spendable is SpenderRole {
+    function spend(address from, uint256 value) public onlySpender returns(bool) {
+        _burn(from, value);
+        return true;
     }
-    
-    function mintingCurrency() external view returns (ERC20Spendable) {
-        return _mintingCurrency;
+}
+
+contract TestToken is SpenderRole {
+    function mint(address to, uint256 value) public returns(bool){
+        _mint(to, value);
+        return true;
     }
-    
-    function setMintingPrice(uint256 newMintingPrice) onlyOwner external {
-        _mintingPrice = newMintingPrice;
-    }
-    
-    function setMintingCurrency(ERC20Spendable newMintingCurrency) onlyOwner external {
-        _mintingCurrency = newMintingCurrency;
-    }
-    
-     function create(bytes32 dataHash) external returns (uint) {
-         
-     }
 }
